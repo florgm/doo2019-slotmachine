31ce496b3d2908623e83c96771f79ab523fc5488
florgm <fgm1212@gmail.com>
2019-05-21 12:46:20 -0300

    Cambios en SlotMachine

--- src/slotmachine/SlotMachine.java
+++ src/slotmachine/SlotMachine.java
@@ -1,4 +1,29 @@
 package slotmachine;
 
+import slotmachine.coinrelated.CoinSlot;
+import slotmachine.coinrelated.DropBox;
+import slotmachine.coinrelated.PayoutTray;
+import slotmachine.gamemode.GameMode;
+import slotmachine.gamemode.GameModeFactory;
+import slotmachine.gamemode.random.RandomFactory;
+import slotmachine.gamemode.sequence.SequenceFactory;
+
 public class SlotMachine {
+    private static SlotMachine instance;
+    private GameMode gamePlayGenerator;
+    private CoinSlot coinSlot;
+    private DropBox dropBox;
+    private PayoutTray payoutTray;
+
+    private SlotMachine() { }
+
+    public static SlotMachine getInstance() {
+        if (instance == null) {
+            instance = new SlotMachine();
+        }
+
+        return instance;
+    }
+
+
 }
